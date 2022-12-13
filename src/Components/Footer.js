8724eb28e93bcb4739c3287071c09306c1e5d016
jcamilorg <juancamilorg64@gmail.com>
2022-12-13 11:10:33 -0500

    refactor next

--- src/Components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => {
-  return (
-    <div className="bg-dark text-white py-2">{new Date().getFullYear()}</div>
-  );
-};
-
-export default Footer;
--- src/Components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Header = () => {
-  return <div className="bg-dark text-white py-2">jcamilorg</div>;
-};
-
-export default Header;
